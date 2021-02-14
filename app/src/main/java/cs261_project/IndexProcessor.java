b94394214024ac0f35235d280bc7a97f44727c15
wildSqurriel <60471431+wildSqurriel@users.noreply.github.com>
2021-02-14 18:03:44 +0000

    All the web pages are now here.
    The web pages are mostly just static html files. You can enter anything into the forms and they will always allow you to go to the next page.

--- app/src/main/java/cs261_project/IndexProcessor.java
+++ app/src/main/java/cs261_project/IndexProcessor.java
@@ -3,11 +3,14 @@ package cs261_project;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ServerWebInputException;
 
 /**
@@ -66,9 +69,48 @@ public class IndexProcessor {
         return "joinEvent";
     }
 
+
+    @PostMapping("/joinEvent")
+    public final String handleJoinEvent() {
+
+        //TODO render feedback template
+
+        return "/feedbackForm";
+
+    }
+
+    @PostMapping("/submitFeedback")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<String> processFeedback(){
+        //TODO process feedback, the attendee needs to stay on the feedbackForm webpage
+        return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+    }
+
     @GetMapping("/hostHomePage")
     public final String servehostHomePage(){
         return "hostHomePage";
+    }
+
+    @GetMapping("/newEventPage")
+    public final String serveCreateEventPage(){
+        return "newEventPage";
+    }
+
+    @GetMapping("/eventsPage")
+    public final String serveEventsPage(){
+        return "eventsPage";
+    }
+
+    @PostMapping("/newEvent")
+    public final String handleNewEvent(){
+        //TODO create new event
+        return "hostHomePage";
+    }
+
+    @GetMapping("/viewFeedback")
+    public final String serveViewFeedback(){
+        //TODO view feedback
+        return "viewFeedbackPage";
     }
 
 }
