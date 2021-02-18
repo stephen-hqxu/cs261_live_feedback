3c0af0bfd885edccf167e3b8998aae927ffc4e4a
PCJY <59020618+PCJY@users.noreply.github.com>
2021-02-18 09:58:25 +0000

    Update Template.java

--- app/src/main/java/cs261_project/data_structure/Template.java
+++ app/src/main/java/cs261_project/data_structure/Template.java
@@ -3,6 +3,7 @@ package cs261_project.data_structure;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.Nullable;
 /**
  * Templates are custom questions from event host.
  * @author Group 12 - Stephen Xu, JunYan Huo, Ellen Tatum, JiaQi Lv, Alexander Odewale
@@ -18,7 +19,7 @@ public class Template {
         public Template mapRow(ResultSet rs, int rowNum) throws SQLException{
 
             Template template = new Template();
-            //Do not call rs.next(), it's automatically managed
+            
             template.ID = rs.getInt("TID");
             template.BelongsTo = rs.getInt("EventID");
             template.Questions = rs.getString("Question");
@@ -44,6 +45,10 @@ public class Template {
     }
 
     //-------------------------------------------//
+
+    public static final RowMapper<Template> getTemplateRowMapper(){
+        return Template.Mapper;
+    }
 
     public int getTemplateID(){
         return this.ID;
