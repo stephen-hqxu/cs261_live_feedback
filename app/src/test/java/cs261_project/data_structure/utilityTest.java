3d0358d29b043c1a9f8d7fbb9167460f6391e984
wildSqurriel <60471431+wildSqurriel@users.noreply.github.com>
2021-02-25 16:17:16 +0000

    More unit tests

--- app/src/test/java/cs261_project/data_structure/utilityTest.java
+++ app/src/test/java/cs261_project/data_structure/utilityTest.java
@@ -5,26 +5,92 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class utilityTest {
+
+
+
     @Test
     @DisplayName("Test should run")
     void shouldRunTest(){
         Assertions.assertEquals("1","1");
     }
 
-    @Test
-    @DisplayName("Sentiment analysis should work")
-    @Disabled("TODO")
-    void sentimentAnalysis(){
-        String case1 = "This is the best ever, I love it so much";
-        String result1 = SentimentAnalyzer.getSentimentType(case1);
-        System.out.println("Mood " + result1);
+    @ParameterizedTest
+    @DisplayName("Sentiment analyzer should return a score of 5 for neutral statements")
+    @ValueSource(strings = {
+            "This is ok.",
+            "It's alright.",
+            "I like it.",
+            "I would come again."
+    })
+    void shouldAnalyseNeutralStatements(String statement){
+        int result = SentimentAnalyzer.getSentimentType(statement);
+        Assertions.assertEquals(5,result);
+    }
 
+
+    @ParameterizedTest
+    @DisplayName("Sentiment analyzer should return a score of 3-5 for negative statements")
+    @ValueSource(strings = {
+            "I hate it.",
+            "This is a waste of time.",
+            "I would not recommend it.",
+            "This workshop is boring."
+    })
+    void shouldAnalyseNegativeStatements(String statement){
+        int score = SentimentAnalyzer.getSentimentType(statement);
+        boolean result = false;
+        if (score <= 5 && score >= 3){
+            result = true;
+        }
+        Assertions.assertTrue(result);
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("Sentiment analyzer should return a score of 5-7 for positive statements")
+    @ValueSource(strings = {
+            "I love it.",
+            "This workshop was very useful.",
+            "I would recommend it to others",
+            "This event is interesting."
+    })
+    void shouldAnalysePositiveStatements(String statement){
+        int score = SentimentAnalyzer.getSentimentType(statement);
+        boolean result = false;
+        if (score <= 7 && score >= 5){
+            result = true;
+        }
+        Assertions.assertTrue(result);
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("Sentiment analyzer should return a score of 1-3 for very negative statements")
+    @ValueSource(strings = {
+            "This is the worst event I have ever attended, It was so boring!",
+            "I almost died of boredom.",
+    })
+    void shouldAnalyseVeryNegativeStatements(String statement){
+        int score = SentimentAnalyzer.getSentimentType(statement);
+        boolean result = false;
+        if (score <= 3 && score >= 1){
+            result = true;
+        }
+        Assertions.assertTrue(result);
+    }
+
+
+    @Test
+    void test(){
+        int r = SentimentAnalyzer.getSentimentType("This is the worst event I have ever attended, It was so boring!");
+        System.out.println(r);
         Assertions.assertTrue(true);
     }
+
+
 }
